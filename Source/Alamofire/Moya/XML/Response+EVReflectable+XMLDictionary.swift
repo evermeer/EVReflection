236f244f764b2cf94fb86a4ba4a3292afddcb128
Edwin Vermeer <edwin@evict.nl>
2017-01-10 21:23:29 +0100

    untested extension

--- Source/Alamofire/Moya/XML/Response+EVReflectable+XMLDictionary.swift
+++ Source/Alamofire/Moya/XML/Response+EVReflectable+XMLDictionary.swift
@@ -9,12 +9,12 @@ import Foundation
 import Moya
 import Xml2Dictionary
 
-open extension Response {
+public extension Response {
     
     /// Maps data received from the signal into an object which implements the EVReflectable protocol.
     /// If the conversion fails, the signal errors.
     public func mapXml<T: EVReflectable>(to type:T.Type) throws -> T where T: NSObject {
-        return map(from: XMLDictionary.dictionaryWithXMLData(data: data))
+        return map(from: Xml2Dictionary.dictionaryWithXMLData(data: data))
     }
 }
 
