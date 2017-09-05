ef17e03327634da3850eca692e12b3388dbb3187
Justin Stanley <justin.stanley@gmail.com>
2017-09-04 21:33:20 -0500

    Added Carthage support for the EVReflection/CloudKit subspec.

---
+++ Carthage/EVReflection_CloudKit/EVReflection_CloudKit.h
@@ -0,0 +1,19 @@
+//
+//  EVReflection_CloudKit.h
+//  EVReflection_CloudKit
+//
+//  Created by Justin Stanley on 2017-09-04.
+//  Copyright © 2017 evict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EVReflection_CloudKit.
+FOUNDATION_EXPORT double EVReflection_CloudKitVersionNumber;
+
+//! Project version string for EVReflection_CloudKit.
+FOUNDATION_EXPORT const unsigned char EVReflection_CloudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EVReflection_CloudKit/PublicHeader.h>
+
+
