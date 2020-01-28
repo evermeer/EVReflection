8f289aba806f4b4e5b00a6c0837bd7f2415fa956
Axel Cantor <nemexyz@gmail.com>
2020-01-28 11:41:52 -0500

    Add Package.swift for SPM

---
+++ Package.swift
@@ -0,0 +1,21 @@
+//  swift-tools-version:5.0
+//
+//  Package.swift
+//  EVReflection
+//
+//  Created by Axel Cantor on 28/01/20.
+//  Copyright © 2015 evict. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "EVReflection",
+                      platforms: [.macOS(.v10_11),
+                                  .iOS(.v8),
+                                  .tvOS(.v9),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "EVReflection",
+                                          targets: ["EVReflection"])],
+                      targets: [.target(name: "EVReflection",
+                                        path: "Source")],
+                      swiftLanguageVersions: [.v5])
